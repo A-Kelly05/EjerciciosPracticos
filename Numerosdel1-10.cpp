1201bb3b15dc8a4695d343edbb051f13c6eb39e3
A-Kelly05 <aidan.kelly16020@est.uca.edu.ni>
2023-05-19 00:38:52 -0600

    Se subio el primer ejercicio de la guia

---
+++ Numerosdel1-10.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int opc=0, Max =11;
+    cout<<"Quieres que esciba los numeros del 1 al 10?\n";
+    cout <<" 1 = si, 2 = no\n";
+    cout << "Digite la respuesta: ";
+    cin >> opc;
+    if (opc=1)
+    {
+       for (int i = 1; i < 11; i++)
+       {
+        cout<< i<<endl;
+       }
+       
+    }else
+    {
+        cout<<"Okey, vuelve pronto"<<endl;
+    }
+   
+    
+    
+    return 0;
+}
