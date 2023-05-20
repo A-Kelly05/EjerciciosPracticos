afa78af51fa0f5582811f535c7a186f72d6d6817
A-Kelly05 <aidan.kelly16020@est.uca.edu.ni>
2023-05-19 21:54:26 -0600

    Cambio en una letra mal colocada

--- Numerosdel1-10.cpp
+++ Numerosdel1-10.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     int opc=0, Max =11;
-    cout<<"Quieres que esciba los numeros del 1 al 10?\n";
+    cout<<"Quieres que escriba los numeros del 1 al 10?\n";
     cout <<" 1 = si, 2 = no\n";
     cout << "Digite la respuesta: ";
     cin >> opc;
